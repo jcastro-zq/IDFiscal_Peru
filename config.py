826fe8b624e08bf53d9bff39406463fb4187f871
jcastro-zq <jcastro@zeroq.cl>
2025-05-13 13:23:53 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class Config(object):
     DEBUG = False
     TESTING = False
-    TESSERACT_ROUTE = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    TESSERACT_ROUTE = "/usr/bin/tesseract"
     SUNAT_URL_IMG = 'https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/captcha?accion=image&magic=2'
     SUNAT_URL_INFO = 'http://www.sunat.gob.pe/cl-ti-itmrconsruc/jcrS00Alias?accion=consPorRuc&nroRuc={0}&codigo={1}'
 
